197669a5ca013c50f890d50a286890852bdd064b
Andrea-Baggio <andreabaggio800@gmail.com>
2023-03-15 16:55:19 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+string[] foods = { "pizza", "cioccolata", "sushi", "hamburger", "pollo", "gelato", "sabbia" };
+
+int arrLen = foods.Length;
+
+for (int i = 0; i < arrLen; i++)
+{
+    Console.WriteLine(foods[i]);
+}
+
+for (int i = 0; i < arrLen; i++)
+{
+    if (i == 0)
+    {
+        Console.WriteLine(foods[i] + ": primo");
+    }
+    else if (i == arrLen / 2)
+    {
+        Console.WriteLine(foods[i] + ": medio");
+    }
+    else if (i == arrLen - 1)
+    {
+        Console.WriteLine(foods[i] + ": ultimo");
+    }
+}
+
+
+Console.WriteLine("La classica comprende " + arrLen + " elementi");
