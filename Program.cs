2f2d2b0922f15ba82d021e6c71a9ab9adee962d8
Andrea-Baggio <andreabaggio800@gmail.com>
2023-03-15 23:09:55 +0100

    bonus sbagliato

--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 
-string[] foods = { "pizza", "cioccolata", "sushi", "hamburger", "pollo", "gelato", "sabbia" };
+string[] foods = { "pizza", "cioccolata", "sushi", "hamburger", "gatti", "pollo", "gelato", "sabbia" };
 
 int arrLen = foods.Length;
+int midElements = arrLen / 2;
 
 for (int i = 0; i < arrLen; i++)
 {
     Console.WriteLine(foods[i]);
 }
+
 
 for (int i = 0; i < arrLen; i++)
 {
@@ -15,15 +17,25 @@ for (int i = 0; i < arrLen; i++)
     {
         Console.WriteLine(foods[i] + ": primo");
     }
-    else if (i == arrLen / 2)
-    {
-        Console.WriteLine(foods[i] + ": medio");
-    }
     else if (i == arrLen - 1)
     {
         Console.WriteLine(foods[i] + ": ultimo");
     }
+    else if (i == arrLen / 2)
+    {
+        Console.WriteLine(foods[i] + ": medio");
+    }
 }
 
 
-Console.WriteLine("La classica comprende " + arrLen + " elementi");
+if (arrLen % 2 == 0)
+{
+    int firstMid = midElements - 1;
+    int secondMid = midElements;
+
+    Console.WriteLine(firstMid + ": medio");
+    Console.WriteLine(secondMid + ": medio");
+}
+
+
+Console.WriteLine("La classifica comprende " + arrLen + " elementi");
